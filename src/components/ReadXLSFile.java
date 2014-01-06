76c9dc9df4728feff0db147495463d52925e9e4b
alterone <brak2sensu>
2014-01-06 16:44:02 +0100

    Clean and build

--- src/components/ReadXLSFile.java
+++ src/components/ReadXLSFile.java
@@ -1,14 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package components;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.Iterator;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
